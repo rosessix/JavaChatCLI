f9335b9642d75d734cbe81826834bb2fb055dae2
rosessix <imlegacy69@gmail.com>
2023-09-20 11:29:13 +0200

    innit bruv

--- javachat/src/Client.java
+++ javachat/src/Client.java
@@ -1,3 +1,42 @@
-public class Client {
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
+public class Client {
+    public static void main(String[] args) {
+        final int PORT = 8080;
+        final String HOST = "localhost";
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hvad vil du have dit brugernavn skal være?: ");
+        String userName = input.next();
+
+        try (Socket socket = new Socket(HOST, PORT)) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+
+            out.println(userName);
+            out.flush();
+
+            sendThread(in, out);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static void sendThread(BufferedReader br, PrintWriter pw) {
+        Scanner input = new Scanner(System.in);
+        new Thread(() -> {
+            while (true) {
+                System.out.println("Dig: ");
+                String msg = input.nextLine();
+                System.out.println("Sender besked: " + msg);
+                pw.println(msg);
+                pw.flush();
+            }
+        }).start();
+    }
 }
--- javachat/src/Server.java
+++ javachat/src/Server.java
@@ -1,3 +1,54 @@
-public class Server {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    public static void main(String[] args) throws IOException {
+        final int PORT = 8080;
+        ServerSocket server = new ServerSocket(PORT);
+
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Socket socket = server.accept();
+                    UserThread user = new UserThread(socket);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        }).start();
+
+    }
+
+    static class UserThread {
+        private Socket socket;
+        private String userName;
+        private BufferedReader br;
+        private PrintWriter pw;
+        public UserThread(Socket socket) throws IOException {
+            this.socket = socket;
+            this.br = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            this.pw = new PrintWriter(this.socket.getOutputStream(), true);
+            System.out.println("trying to read line?");
+
+            this.userName = this.br.readLine();
+            System.out.println(this.userName);
+
+            this.recieveThread(this.br, this.pw);
+
+        }
+
+        private void recieveThread(BufferedReader br, PrintWriter pw) throws IOException {
+            String msg;
+            while (true) {
+                msg = br.readLine();
+                if (msg != null)
+                    System.out.println("msg " + msg);
+            }
+        }
+    }
 }
